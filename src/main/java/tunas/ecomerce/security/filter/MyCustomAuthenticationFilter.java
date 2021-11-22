b391a024f9e538ebf4049acab295f34309cd9f22
ramdan <ramadanariadi@gmail.com>
2021-11-22 16:26:29 +0800

    update token expired time

--- src/main/java/tunas/ecomerce/security/filter/MyCustomAuthenticationFilter.java
+++ src/main/java/tunas/ecomerce/security/filter/MyCustomAuthenticationFilter.java
@@ -74,13 +74,13 @@ public class MyCustomAuthenticationFilter extends UsernamePasswordAuthentication
         Algorithm algorithm = TokenGenerationAlgorithm.algorithm;
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 5 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 6 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
         Map<String, String> tokens = new HashMap<>();
