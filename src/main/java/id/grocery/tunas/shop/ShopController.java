c6ec982f01b021e088f0e87c94f3eb75fa1bfed8
Ramdan Ariadi <ramadanariadi@gmail.com>
2023-08-19 00:00:06 +0800

    shop service

---
+++ src/main/java/id/grocery/tunas/shop/ShopController.java
@@ -0,0 +1,48 @@
+package id.grocery.tunas.shop;
+
+import id.grocery.tunas.shop.dto.GetShopDTO;
+import id.grocery.tunas.shop.dto.ShopDTO;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/shop")
+@AllArgsConstructor
+public class ShopController {
+
+    private ShopService shopService;
+
+    @PostMapping
+    public ResponseEntity<Object> addStore(HttpServerRequest request, @RequestBody ShopDTO shop){
+        JsonObject userCustomId = new JsonObject(request.getHeader("x-custom-id"));
+        shopService.saveShop(UUID.fromString(userCustomId.getString("userID")), shop);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getStore(HttpServerRequest request){
+        JsonObject userCustomId = new JsonObject(request.getHeader("x-custom-id"));
+        GetShopDTO.Response response = shopService.getUserShop(UUID.fromString(userCustomId.getString("userId")));
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateStore(HttpServerRequest request, @PathVariable("id") UUID id, @RequestBody ShopDTO shop){
+        JsonObject userCustomId = new JsonObject(request.getHeader("x-custom-id"));
+        shopService.updateShop(UUID.fromString(userCustomId.getString("userId")), id, shop);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteStore(HttpServerRequest request, @PathVariable("id") UUID id){
+        JsonObject userCustomId = new JsonObject(request.getHeader("x-custom-id"));
+        shopService.deleteShop(UUID.fromString(userCustomId.getString("userId")), id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/id/grocery/tunas/shop/dto/GetShopDTO.java
@@ -0,0 +1,24 @@
+package id.grocery.tunas.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+public class GetShopDTO {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response{
+        public ShopDTO data;
+    }
+
+    @Data
+    public static class ShopDTO {
+        private UUID id;
+        private String name;
+        private String imageUrl;
+        private String address;
+    }
+}
